1002ec2d83cb0362619f00703028364847fd147d
Dahee Jung <daheejung419@gmail.com>
2018-07-18 23:02:15 -0600

    practice-components

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Blog from './components/Blog'
+
+const App = () => {
+  return (
+    < Blog />
+   
+  )
+}
+
+export default App;
---
+++ src/components/Blog.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './Blog.css'
+
+
+
+const Blog = () => {
+return (
+    <div>
+       <div className="menu">
+            HOME<br />
+            FEED<br />
+            MESSAGE<br />
+       </div>
+       <div>
+           PHOTO
+       </div>
+        
+   
+    </div>
+)
+}
+
+export default Blog;
